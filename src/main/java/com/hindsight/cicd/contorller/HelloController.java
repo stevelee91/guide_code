0b0833cbc5c0940a03eb4b878bf1327e951e2824
stevelee91 <30716535+stevelee91@users.noreply.github.com>
2021-06-28 09:51:40 +0900

    committed 2021-06-28

---
+++ src/main/java/com/hindsight/cicd/contorller/HelloController.java
@@ -0,0 +1,14 @@
+package com.hindsight.cicd.contorller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello #4";
+    }
+
+}
